231158cb375d021a28da48ba025a1f7aac6cf2a1
andreeamatacuta <andreea_matacuta@yahoo.com>
2015-05-20 19:35:01 +0300

    Update modulFAV.cpp

--- modulFAV.cpp
+++ modulFAV.cpp
@@ -47,7 +47,7 @@ void inserareFAV(NodFAV* listaDublaCirculara,NodFAV* nod) {
 		listaDublaCirculara->prev = nod;
 	}
 }
-void initializareFAV(FILE* file) {
+void initializareFAV(FILE*& file) {
 	if (!file) {
 		printf("Nu se poate deschide fisierul!\n");
 	}
@@ -97,47 +97,29 @@ void findFAV(char* id) {
 	else printf("\nNu exista elementul\n");
 }
 
-void updateFAV(char* DATE) {
-	/*	DE MODIFICAT!!!!!!!!!
-	NodFAV* elem = cautaElementFAV(idAeronava);
-	if (elem) {
-	printf("\nCe vrei sa modifici?\n");
-	printf("1.Id-ul aeronavei\n");
-	printf("2.Numar de locuri\n");
-	printf("3.Greutatea maxima\n");
-	printf("4.Tipul aeronavei\n");
-	int ad;
-	printf("\nIntrodu cifra corespunzatoare alegerii:");
-	scanf("%d", &ad);
-	switch (ad) {
-	case 1:
-	free(elem->infoUtil->idAeronava);
-	printf("\nIntroduceti noul id al aeronavei:");
-	char temp[100];
-	scanf("%s", &temp);
-	elem->infoUtil->idAeronava = (char*)malloc(strlen(temp) + 1);
-	strcpy(elem->infoUtil->idAeronava, temp);
-	break;
-	case 2:
-	printf("\nIntroduceti noul numar de locuri:");
-	int nr;
-	scanf("%d", &nr);
-	elem->infoUtil->nrLocuri = nr;
-	break;
-	case 3:
-	printf("\nIntroduceti noua greutate maxima:");
-	int n;
-	scanf("%d", &n);
-	elem->infoUtil->greutateMaxima = n;
-	break;
-	case 4:
-	if (elem->infoUtil->tipAero == 1)
-	elem->infoUtil->tipAero = elem->infoUtil->PAS;
-	else if (elem->infoUtil->tipAero == 0)
-	elem->infoUtil->tipAero == elem->infoUtil->CAR;
+void updateFAV(char* id,char* Date) {
+	char* da = (char*)malloc(sizeof(char)*strlen(Date));
+	strcpy(da, Date);
+	linieFAV* fav = (linieFAV*)malloc(sizeof(linieFAV));
+	while (listaDublaCirculara->next && (strcmp(listaDublaCirculara->infoUtil->idAeronava, id) != 0))
+				listaDublaCirculara = listaDublaCirculara->next;
+	if (strcmp(listaDublaCirculara->infoUtil->idAeronava, id) == 0)
+	{
+		char* id;
+		char s[] = " ";
+		id = strtok(da, s);
+		//???Conversie! fav->CAR=id;
+		//listaDublaCirculara->infoUtil->CAR =atoi (id);
+		while (id != NULL)
+		{
+			id = strtok(NULL, " ");
+			listaDublaCirculara->infoUtil->nrLocuri = atoi(id);
+			id = strtok(NULL, " ");
+			listaDublaCirculara->infoUtil->greutateMaxima = atof(id);
+			id = strtok(NULL, " ");
+
+		}
 	}
-	} else
-	printf("\nNu exista aceasta aeronava");*/
 }
 
 void deleteFAV(char* id) {
@@ -190,5 +172,7 @@ void main() {
 	afisareFAV();
 	//findFAV("qwer");
 	addFAV("aeronava4 1 34 456");
+	updateFAV("aeronava4", "1 36 345");
+	afisareFAV();
 	
 }
